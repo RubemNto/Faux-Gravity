e1d0371919f4d2ba08bff6b1f9726d8eab839e75
Rubem Fridolino Christ Neto <56802722+RubemNto@users.noreply.github.com>
2020-12-07 10:19:29 +0000

    Add files via upload

---
+++ GravityBody.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityBody : MonoBehaviour
+{
+    private Rigidbody rb;
+    public GravityController controller;
+   
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+        rb.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        controller.Attract(transform);        
+    }
+}
---
+++ GravityController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour
+{
+    public float gravity = -10;
+
+    public void Attract(Transform body)
+    {
+        Vector3 gravityUp = (body.position-transform.position).normalized;//vector pointing to center of planet
+        Vector3 bodyUp = body.up;//vector pointing up of the object we control
+
+        body.GetComponent<Rigidbody>().AddForce(gravityUp*gravity);//force pointing to the center of the planet
+
+        Quaternion targetRotation = Quaternion.FromToRotation(bodyUp,gravityUp)*body.rotation;//calculation for rotation to be made
+        body.rotation = Quaternion.Slerp(body.rotation,targetRotation,50*Time.deltaTime);//rotating the player 
+
+    }
+}
